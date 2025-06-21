39c1b06c7df55f17f41f36fb4a91575629627e29
faiza.nb <faizanb.mec@gmail.com>
2025-06-21 22:05:24 +0530

    logo updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import React, {memo} from 'react';
-import logoImg from '../assets/quiz-logo.jpg';
 
 const Header: React.FC = memo(() => {
     return (
         <header>
-            <img src={logoImg} alt={"Quiz Logo"} />
+            <img src='/quiz-logo.jpg' alt={"Quiz Logo"} />
             <h1>TeQuiz</h1>
         </header>
     )
