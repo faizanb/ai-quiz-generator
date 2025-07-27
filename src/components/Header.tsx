ec9eae4b04a812d6ddc3da407566a2e5ee1ebd24
faiza.nb <faizanb.mec@gmail.com>
2025-07-27 13:52:51 +0530

    vite production ready build steps updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React, {memo} from 'react';
+import React, { memo } from "react";
 
 const Header: React.FC = memo(() => {
-    return (
-        <header>
-            <img src='/quiz-logo.jpg' alt={"Quiz Logo"} />
-            <h1>TeQuiz</h1>
-        </header>
-    )
-})
+  return (
+    <header>
+      <img src={`${import.meta.env.BASE_URL}quiz-logo.jpg`} alt={"Quiz Logo"} />
+      <h1>TeQuiz</h1>
+    </header>
+  );
+});
 
 export default Header;
